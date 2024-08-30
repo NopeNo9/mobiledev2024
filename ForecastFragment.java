d71d0792eff0b8501b47cf650dfccbebbf59a17d
NopeNo9 <bachphamchi@gmail.com>
2024-08-30 13:54:48 +0700

    after move in git file

---
+++ ForecastFragment.java
@@ -0,0 +1,22 @@
+package vn.edu.usth.weather.mobiledev2024;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vn.edu.usth.weather.R;
+
+public class ForecastFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        view.setBackgroundColor(0x20FF0000);
+        return view;
+    }
+}
